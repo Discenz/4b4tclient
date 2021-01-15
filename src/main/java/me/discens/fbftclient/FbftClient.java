c0a8691a4e043e7c8242b80d63ee2733b1df7fe7
Discenz <dragoneyedevelopment@gmail.com>
2021-01-15 23:02:47 +0100

    Add alpine, implement logger

--- src/main/java/me/discens/fbftclient/FbftClient.java
+++ src/main/java/me/discens/fbftclient/FbftClient.java
@@ -1,14 +1,20 @@
 package me.discens.fbftclient;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class FbftClient implements ModInitializer {
+
+	public static final String NAME = "4b4t Client";
+	public static final String VERSION = "1.0.0";
+
+	public static Logger logger;
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		logger = LogManager.getLogger("4b4t");
 
-		System.out.println("Hello Fabric world!");
+		logger.info("initializing client");
 	}
 }
